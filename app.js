72f65250626b2ff955a44deaa4e521971d14969b
xmz <shenf220@gmail.com>
2022-01-12 23:51:53 +0000

    ready to deploy

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const compression = require('compression')
+
+const app = express()
+
+app.use(compression())
+app.use(express.static('./dist'))
+
+app.listen(80, ()=> {
+    console.log('web server is running at http://127.0.0.1')
+})
